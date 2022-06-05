1da54deaf6a1f1494ebf0c60cebcdd2e696d7857
bakkeshks <bakkeshpruthvi49@gmail.com>
2022-06-06 01:03:36 +0530

    Fix : used multi line comment

--- Week-3/Assignment 3.3/index.js
+++ Week-3/Assignment 3.3/index.js
@@ -16,26 +16,19 @@ increnment();                                       // call the increnment() fun
 increnment();                                       // call the increnment() function
 log();                                              // call the log()  function
 
-// OUTPUT : Count is O (zero)
-// STEPS : why count is 0 not 3 
-//  1] The program begin calling from line no 13 which is createincrement() function and set the count variable  value to 0.
-//  2] After setting count value to 0 we call increnment function 3 times.
-//  3] 1'st Iteration count = 0 and increnment count by 1 and value count is 1
-//  4] 2'st Iteration count = 1 and increnment count by 1 and value count is 2
-//  5] 3'st Iteration count = 2 and increnment count by 1 and value count is 3
-//  4] when we call log() function in the line no 7 it will check message variable in the inner scope 
-//     since the variable is outer scope it will print the value as "COUNT IS 0".
-//  5] Since the outer variable or function do not have access to inner function it will return count as 0 inside of 
-//    count as 3 which was there in increnment() function .
-//        
-//      CLOSURE : closures are functions that can access values outside the block and the outer function cannot
-//                  have access to inner function
-//
-//
-//
-//
+/* OUTPUT : Count is O (zero)
+ STEPS : why count is 0 not 3 
+   1] The program begin calling from line no 13 which is createincrement() function and set the count variable  value to 0.
+   2] After setting count value to 0 we call increnment function 3 times.
+   3] 1'st Iteration count = 0 and increnment count by 1 and value count is 1
+   4] 2'st Iteration count = 1 and increnment count by 1 and value count is 2
+   5] 3'st Iteration count = 2 and increnment count by 1 and value count is 3
+   4] when we call log() function in the line no 7 it will check message variable in the inner scope 
+      since the variable is outer scope it will print the value as "COUNT IS 0".
+   5] Since the outer variable or function do not have access to inner function it will return count as 0 inside of 
+     count as 3 which was there in increnment() function .
+         
+       CLOSURE : closures are functions that can access values outside the block and the outer function cannot
+                    have access to inner function
 
-
-
-
-
+*/
