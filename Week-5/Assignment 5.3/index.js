57e2687861d7554952383a9c514da20e2879893f
bakkeshks <bakkeshpruthvi49@gmail.com>
2022-06-15 01:59:17 +0530

    Comments added

--- Week-5/Assignment 5.3/index.js
+++ Week-5/Assignment 5.3/index.js
@@ -1,6 +1,4 @@
-function hasDuplicate(arr) {
-    /* hasDuplicate function which check if the element 
-                                    in the array contain duplicate element */
+function hasDuplicate(arr) {                   // hasDuplicate function which check if the element  in the array contain duplicate element
     const arr2 = Array.from(new Set(numbers)); // arr2 set which stores the unique values and remove duplicate 
     if (arr.length == arr2.length) {
         return false;
@@ -9,9 +7,9 @@ function hasDuplicate(arr) {
     }
 
 }
-const numbers = [1, 2, -1]; // array elements
+const numbers = [1, 2, -1];             // array elements
 const result = hasDuplicate(numbers);
-/* hasduplicate function will  return true if the elements in the
+                                        /* hasduplicate function will  return true if the elements in the
                                         array  contains duplicate or return false  if all elements are unique */
 console.log(result);
 
