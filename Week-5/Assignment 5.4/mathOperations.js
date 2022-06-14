fa914a93120466b68ff02ea80bf026904de21708
bakkeshks <bakkeshpruthvi49@gmail.com>
2022-06-15 01:30:03 +0530

    Comments added

--- Week-5/Assignment 5.4/mathOperations.js
+++ Week-5/Assignment 5.4/mathOperations.js
@@ -1,12 +1,26 @@
 const mathOperations = {
-  sum: function(a,b) {
+  sum: function (a, b) {        // Sum Method which will return a + b
     return a + b;
   },
-  diff: function(a,b) {
+  diff: function (a, b) {       // diff Method which will return a - b
     return a - b;
   },
-  product: function(a,b) {
+  product: function (a, b) {    // product Method which will return a * b
     return a * b;
   },
 }
-module.exports = mathOperations
+module.exports = mathOperations  
+
+
+/* OUTPUT : 
+  PASS ./mathoperation.test.js
+  adds 1 + 4 to equal 5 (3 ms)
+  diff 4 - 2 return 2 (1 ms)
+  multiply 2 * 3 require 6 (1 ms)
+
+  Test Suites : 1 Passed, 1 total
+  Tests :       3 passed, 3 total
+  Snapshots :   0 total
+  Time :        0.557 s
+  Ran all test suites.
+*/
--- Week-5/Assignment 5.4/mathOperations.test.js
+++ Week-5/Assignment 5.4/mathOperations.test.js
@@ -1,7 +1,8 @@
-const mathOperations = require('./mathOperations');
+const mathOperations = require('./mathOperations');      // import mathOperations javascript file 
 
-test('adds 1 + 4 to equal 5', () => {
-  expect(mathOperations.sum(1, 4)).toBe(5);
+test('adds 1 + 4 to equal 5', () => {                   /* test function accepts 2 parameter one for displaying 
+                                                        user defined message and second one is function  */
+  expect(mathOperations.sum(1, 4)).toBe(5);                   
 });
 test('diff 4 - 2 return 2 ', () => {
   expect(mathOperations.diff(4, 2)).toBe(2);
@@ -9,3 +10,4 @@ test('diff 4 - 2 return 2 ', () => {
 test('multiply 2 * 3 require 6', () => {
   expect(mathOperations.product(2, 3)).toBe(6);
 });
+
